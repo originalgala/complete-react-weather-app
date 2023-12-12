622e7bbb96b8a9afe524cb7f91b62dece1189e4d
originalgala <133504883+originalgala@users.noreply.github.com>
2023-12-12 11:21:07 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <div className="container">
+        <Weather />
+      </div>
     </div>
   );
 }

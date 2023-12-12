fa633df5a13e8e65838f1664e4f6ee435cbbd66e
originalgala <133504883+originalgala@users.noreply.github.com>
2023-12-12 14:09:36 +0200

    Added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,26 +4,31 @@ import "./Weather.css";
 export default function Weather () {
     return(
         <div className="Weather">
-            
-            <input type="search" placeholder="Enter a city" />
-            <input type="submit" value="search" className="btn btn-success" />
-            <h1>Tokyo</h1>
+            <form>
+            <input type="search" placeholder="Enter a city..." autoFocus="on" />
+            <input type="submit" value="search" className="bg-success bg-gradient submit-button" />
+            </form>
+
+            <h1 className="mt-3">Tokyo</h1>
             <h5>Tuesday 11:52</h5>
             <ul className="mainWeather">
-                <li> <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
-                alt="mostly sunny"/></li>
+               <li> <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+                alt="mostly sunny"/>
+                </li>
+               <li className="temp fs-1">
+                22°C</li> 
                 <li>Mostly sunny</li>
-                <li>22°C</li>
+                
             </ul>
 
             <div className="row">
-                <div className="col-4">
+                <div className="col-4 text-end">
                     <p>feels like 22°C</p>
                 </div>
                 <div className="col-4">
                     <p>Wind:8km/h</p>
                 </div>
-                <div className="col-4">
+                <div className="col-4 text-start">
                     <p>Humidity:15%</p>
                 </div>
             </div>

fc5058d29849609e91681158e8708ca7660c4a10
originalgala <133504883+originalgala@users.noreply.github.com>
2023-12-12 12:19:41 +0200

    Added basic HTML structure

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather />
+       <footer> Coded by Galaletsang Tlholoe and is <a href="https://github.com/originalgala/complete-react-weather-app" >
+        open-sourced on Github</a>
+      </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,33 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather () {
     return(
-        <div>
-            <h1>Hello Weather</h1>
+        <div className="Weather">
+            
+            <input type="search" placeholder="Enter a city" />
+            <input type="submit" value="search" className="btn btn-success" />
+            <h1>Tokyo</h1>
+            <h5>Tuesday 11:52</h5>
+            <ul className="mainWeather">
+                <li> <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" 
+                alt="mostly sunny"/></li>
+                <li>Mostly sunny</li>
+                <li>22°C</li>
+            </ul>
+
+            <div className="row">
+                <div className="col-4">
+                    <p>feels like 22°C</p>
+                </div>
+                <div className="col-4">
+                    <p>Wind:8km/h</p>
+                </div>
+                <div className="col-4">
+                    <p>Humidity:15%</p>
+                </div>
+            </div>
+
         </div>
     )
 }
